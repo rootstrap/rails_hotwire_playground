b779dd0eb05413653114051c4cae3fb0e5c25c0b
Bruno Vezoli <bruno.vezoli@rootstrap.com>
2021-02-03 12:43:14 -0300

    Catch up with base (#2)

--- spec/system/users/sign_in_spec.rb
+++ spec/system/users/sign_in_spec.rb
@@ -5,31 +5,43 @@ require 'rails_helper'
 feature 'User sign in' do
   let(:email) { Faker::Internet.email }
   let(:password) { Faker::Internet.password(min_length: 8) }
-  let!(:user) { create(:user, email: email, password: password) }
+  let(:sign_in_page) { PageObjects::SignIn.new }
 
-  feature 'with correct credentials' do
-    scenario 'signs in successfully' do
-      visit new_user_session_path
+  feature 'when user has confirmed account' do
+    let!(:user) { create(:user, email: email, password: password) }
 
-      fill_in 'Email', with: email
-      fill_in 'Password', with: password
-      click_on 'Log in'
+    feature 'with correct credentials' do
+      scenario 'signs in successfully' do
+        sign_in_page.visit_page
+        sign_in_page.fill_in_with(email, password)
+        sign_in_page.submit
 
-      expect(page).to have_text('Signed in successfully')
-      expect(page).to have_current_path(root_path)
+        expect(sign_in_page).to have_success_message
+        expect(page).to have_current_path(root_path)
+      end
+    end
+
+    feature 'with incorrect credentials' do
+      scenario 'fails to sign in' do
+        sign_in_page.visit_page
+        sign_in_page.fill_in_with(email, 'wrong_password')
+        sign_in_page.submit
+
+        expect(sign_in_page).to have_invalid_credentials_error_message
+        expect(page).to have_current_path(new_user_session_path)
+      end
     end
   end
 
-  feature 'with incorrect credentials' do
+  feature 'when user has unconfirmed account' do
+    let!(:user) { create(:user, email: email, password: password, confirmed_at: nil) }
+
     scenario 'fails to sign in' do
-      visit new_user_session_path
+      sign_in_page.visit_page
+      sign_in_page.fill_in_with(email, password)
+      sign_in_page.submit
 
-      fill_in 'Email', with: email
-      fill_in 'Password', with: 'incorrect password'
-      click_on 'Log in'
-
-      expect(page).to have_text('Invalid Email or password')
-      expect(page).to have_current_path(new_user_session_path)
+      expect(sign_in_page).to have_unconfirmed_email_error_message
     end
   end
 end
